da2045daef4879298b03708baf2c9cdfafb6b462
Mihaela Marson <mitchmarson666@gmail.com>
2022-01-16 00:11:29 +0100

    new features

--- script.js
+++ script.js
@@ -1,79 +1,88 @@
 $(() => {
-  let email, confPassword, password, phone = ""
-  const visible = $("#visible")
-  const confVisible = $("#conf-visible")
-  const emailValidation = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/
-  const passwordValidation = /^(?=.*[a-z])(?=.*[0-9])(?=.{8,})/
-  const phoneValidation = /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/im
-  let togglePasswordV = false
-  let toggleConfPasswordV = false
-  const validateMsg = $(".right-box__validate")
-  const URL = "https://613dbc7b94dbd600172ab9bf.mockapi.io/login"
+	let email = (confPassword = password = phone = "");
+	const visible = $("#visible");
+	const confVisible = $("#conf-visible");
+	const emailValidation =
+		/[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/;
+	const passwordValidation = /^(?=.*[a-z])(?=.*[0-9])(?=.{8,})/;
+	const phoneValidation =
+		/^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/im;
+	let togglePasswordV = false;
+	let toggleConfPasswordV = false;
+	const validateMsg = $(".right-box__validate");
+	const URL = "https://613dbc7b94dbd600172ab9bf.mockapi.io/login";
 
+	const toggleVisibility = (item, toggle, password, img) => {
+		item.click(() => {
+			toggle = toggle ? false : true;
+			password.attr("type", toggle ? "text" : "password");
+			img.attr(
+				"src",
+				toggle ? "./img/visibility-on.svg" : "./img/visibility_off.svg"
+			);
+		});
+	};
 
-  const toggleVisibility = (item, toggle, password, img) => {
-    item.click(() =>{
-      toggle = toggle ? false : true
-      password.attr("type", toggle ? "text" : "password")
-      img.attr("src", toggle ? "./img/visibility-on.svg" : "./img/visibility_off.svg")
-    })
-  }
+	const validate = (input, text) => {
+		$(input).addClass("required");
+		validateMsg.text(text);
+		$(input).click(function () {
+			$(this).removeClass("required");
+			validateMsg.text("");
+		});
+	};
 
-  const validate = (input, text) => {
-    $(input).addClass("required")
-    validateMsg.text(text)
-    $(input).click(function(){
-      $(this).removeClass("required")
-      validateMsg.text("")
-    })
-  }
-  
-  $("input").change(() => {
-    password = $("#password").val()
-    confPassword = $("#conf-password").val()
-    email = $("#email").val()
-    phone = $("#phone").val()
-  })
+	$("input").change(() => {
+		password = $("#password").val();
+		confPassword = $("#conf-password").val();
+		email = $("#email").val();
+		phone = $("#phone").val();
+	});
 
-  $("form#signup").submit((e) =>{
-    e.preventDefault()
-    
-    //  validation --------------
-    $("section").removeClass("success")
-    if(!email){
-      validate("#email", "Email required")
-      
-    }else if(!email.match(emailValidation)){
-      validate("#email", "Email not valid")
-    }else if(!password){
-      validate("#password", "Password required")
-    }else if(!password.match(passwordValidation)){
-      validate("#password", "Password not valid")
-    }else if(password !== confPassword){
-      validate("#conf-password", "Password doesn't match")
-    }else{
-      $.ajax(URL,{
-        type:"POST",
-        dataType:"json",
-        data:{
-          email,
-          password,
-          phone
-        }, 
-        success: function(data, status, jqXHR){
-          $("section").addClass("success")
-          $(".right-box__loged-in h2").text(`You are loged in , Welcome ${email}`)
-        },
-        error: (jqXHR, status, throwErr) => console.log("fail")
-      })
-      
-    }
-  })
+	$("form#signup").submit((e) => {
+		e.preventDefault();
 
-  toggleVisibility(visible, togglePasswordV, $("#password"), visible)
-  toggleVisibility(confVisible, toggleConfPasswordV, $("#conf-password"), confVisible)
+		//  validation --------------
+		$("section").removeClass("success");
+		if (!email) {
+			validate("#email", "Email required");
+		} else if (!email.match(emailValidation)) {
+			validate("#email", "Email not valid");
+		} else if (!password) {
+			validate("#password", "Password required");
+		} else if (!password.match(passwordValidation)) {
+			validate("#password", "Password not valid");
+		} else if (password !== confPassword) {
+			validate("#conf-password", "Password doesn't match");
+		} else {
+			$.ajax(URL, {
+				type: "POST",
+				dataType: "json",
+				data: {
+					email,
+					password,
+					phone,
+				},
+				success: function (data, status, jqXHR) {
+					$("section").addClass("success");
+					$(".right-box__loged-in h2").text(
+						`You are loged in , Welcome ${email.split("@")[0]}`
+					);
+				},
+				error: (jqXHR, status, throwErr) => console.log("fail"),
+			});
+		}
+	});
 
+	toggleVisibility(visible, togglePasswordV, $("#password"), visible);
+	toggleVisibility(
+		confVisible,
+		toggleConfPasswordV,
+		$("#conf-password"),
+		confVisible
+	);
 
-  window.location.pathname == "/index.html" ? $("#signup").css("color", "#AB520C")
-  :$("#login").css("color", "#AB520C")
-})
+	window.location.pathname == "/index.html" || "/"
+		? $("#signup").css("color", "#AB520C")
+		: $("#login").css("color", "#AB520C");
+});
---
+++ weather.js
@@ -0,0 +1,30 @@
+$(() => {
+	$.getScript("./config.js");
+	const API_KEY = config.MY_KEY;
+
+	$("form#get-weather").submit(function (e) {
+		e.preventDefault();
+		let city = "";
+		let html = "";
+		const dataDiv = $("div.data");
+		city = $("input#city").val();
+		const API_URL = `https://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=${city}&aqi=no`;
+		if (city !== "") {
+			$.get(API_URL, function (data) {
+				console.log(data);
+				let { current, location } = data;
+				html = `
+        <div class="data-temp">
+          <p>${current.temp_c}°</p>
+          <img src="${current.condition.icon}" />
+        </div>
+        <p>Longitude: ${location.lon}</p>
+        <p>Latitude: ${location.lat}</p> 
+        `;
+				dataDiv.empty().append(html);
+			}).fail(() => dataDiv.empty().append("<p>Data not found</p>"));
+		} else {
+			dataDiv.empty().append("<p>Please enter city name</p>");
+		}
+	});
+});
